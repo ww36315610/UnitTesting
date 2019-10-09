316d2c895f24914f583795e9b36a653a96a2c3dd
wangjian02 <wangjian02@finupgroup.com>
2019-10-09 15:08:08 +0800

    test

--- src/main/java/com/test/Debug.java
+++ src/main/java/com/test/Debug.java
@@ -1,4 +1,17 @@
 package com.test;
 
 public class Debug {
+    public void test1(String name){
+        name = "ttt111";
+        System.out.println(name);
+    }
+
+    public String test2(){
+        return "tt222";
+    }
+
+    public static void main(String[] args) {
+        Debug dd = new Debug();
+        dd.test1(dd.test2());
+    }
 }
