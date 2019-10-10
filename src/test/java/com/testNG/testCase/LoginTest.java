15b5cc617ff1d5b1fd9e957d62ca555ac34fbd76
wangjian02 <wangjian02@finupgroup.com>
2019-10-10 12:01:40 +0800

    add allure and testNG

--- src/test/java/com/testNG/testCase/LoginTest.java
+++ src/test/java/com/testNG/testCase/LoginTest.java
@@ -7,46 +7,45 @@ import org.testng.annotations.Test;
 public class LoginTest {
 
     @Test
-    public void testUserLogin0(){
+    public void testUserLogin0() {
         Login login = new Login();
-        String ac = login.userLogin("","");
-        Assert.assertEquals("用户名或者密码不能为空",ac,"空 is fail");
+        String ac = login.userLogin("", "");
+        Assert.assertEquals("用户名或者密码不能为空", ac, "空 is fail");
     }
 
 
     @Test
-    public void testUserLogin1(){
+    public void testUserLogin1() {
         Login login = new Login();
-        String ac = login.userLogin("admin","");
-        Assert.assertEquals("用户名或者密码不能为空",ac,"admin空 is fail");
+        String ac = login.userLogin("admin", "");
+        Assert.assertEquals("用户名或者密码不能为空", ac, "admin空 is fail");
     }
 
     @Test
-    public void testUserLogin2(){
+    public void testUserLogin2() {
         Login login = new Login();
-        String ac = login.userLogin("zhangsan","");
-        Assert.assertEquals("用户名或者密码不能为空",ac,"zhangsan空 is fail");
+        String ac = login.userLogin("zhangsan", "");
+        Assert.assertEquals("用户名或者密码不能为空", ac, "zhangsan空 is fail");
     }
 
     @Test
-    public void testUserLogin3(){
+    public void testUserLogin3() {
         Login login = new Login();
-        String ac = login.userLogin("","pwd");
-        Assert.assertEquals("用户名或者密码不能为空",ac,"pwd is fail");
+        String ac = login.userLogin("", "pwd");
+        Assert.assertEquals("用户名或者密码不能为空", ac, "pwd is fail");
     }
 
     @Test
-    public void testUserLogin4(){
+    public void testUserLogin4() {
         Login login = new Login();
-        String ac = login.userLogin("admin","adminpwd");
-        Assert.assertEquals("欢迎管理员",ac,"admin pwd is fail");
+        String ac = login.userLogin("admin", "adminpwd");
+        Assert.assertEquals("欢迎管理员", ac, "admin pwd is fail");
     }
 
     @Test
-    public void testUserLogin5(){
+    public void testUserLogin5() {
         Login login = new Login();
-        String ac = login.userLogin("zhangsan","zhangsan");
-        Assert.assertEquals("欢迎zhangsan",ac,"zhangsan pwd is fail");
+        String ac = login.userLogin("zhangsan", "zhangsan");
+        Assert.assertEquals("欢迎zhangsan", ac, "zhangsan pwd is fail");
     }
-
 }
---
+++ src/test/java/com/testNG/testCase/LoginTestParamsData.java
@@ -0,0 +1,38 @@
+package com.testNG.testCase;
+
+import org.testng.Assert;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+
+public class LoginTestParamsData {
+
+    @Test(parameters = {"name", "pwd", "expect"})
+    public void testUserLoginParam(String name, String pwd, String expect) {
+        Login login = new Login();
+        String ac = login.userLogin(name, pwd);
+        Assert.assertEquals(expect, ac);
+    }
+
+
+    @Test(dataProvider = "putUsers", dataProviderClass = LoginTestParamsData.class)
+    public void testUserLoginData(String name,String pwd,String expect) {
+
+        Login login = new Login();
+        String ac = login.userLogin(name, pwd);
+        Assert.assertEquals(expect, ac);
+    }
+
+
+    @DataProvider(name = "putUsers")
+    public Object[][] putUsers() {
+        return new Object[][]{
+                {"", "", "用户名或者密码不能为空"},
+                {"admin", "", "用户名或者密码不能为空"},
+                {"zhangsan", "", "用户名或者密码不能为空"},
+                {"", "pwd", "用户名或者密码不能为空"},
+                {"admin", "123456", "欢迎管理员"},
+                {"zhangsan", "123456", "欢迎zhangsan"}
+        };
+    }
+
+}
