6abd17a539e6c6817eeb82d5cd7101cb4323b9c1
wangjian02 <wangjian02@finupgroup.com>
2019-10-12 17:00:58 +0800

    add allure by dataProvider

--- src/test/java/com/allure/SetStep.java
+++ src/test/java/com/allure/SetStep.java
@@ -1,5 +1,85 @@
 package com.allure;
 
+import io.qameta.allure.Allure;
+import io.qameta.allure.Step;
+import io.qameta.allure.model.Status;
+import io.qameta.allure.model.StepResult;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+
 public class SetStep {
 
+    /**
+     * 动态添加步骤【step】
+     *
+     * @param stepName:步骤名称
+     * @param methodName：动态执行方法名
+     * @param methodParams：动态执行方法-入参
+     */
+    public void stepAdd(String stepName, String methodName, String methodParams) {
+        StepResult stepResult = new StepResult();
+        stepResult.setStatus(Status.FAILED);
+        stepResult.setName(stepName);
+        Allure.getLifecycle().startStep(stepName, stepResult);
+
+        //动态步骤-添加内容
+        try {
+            String getMethodParam = (String) reflectionMethod(methodName, methodParams);
+        } catch (NoSuchMethodException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+
+        Allure.getLifecycle().stopStep();
+    }
+
+    //[静态增加步骤]其他用户登录
+    @Step("loginByother")
+    public String loginByOther(String param) {
+        //上传一张照片，这个可以用来做UI自动化报错是截图，然后传到报告中去
+        pushPicture();
+        param = "这是其他的用户在登录测试……";
+        return param;
+    }
+
+    //[静态增加步骤]admin登录
+    @Step("loginByAdmin")
+    public String loginByAdmin(String param) {
+        //上传一张照片，这个可以用来做UI自动化报错是截图，然后传到报告中去
+        pushPicture();
+        param = "这是admin在登录测试……";
+        System.out.println(param);
+        return param;
+    }
+
+    //[静态增加步骤]zhangsan登录
+    @Step("loginByZhangSan")
+    public String loginByZhangSan(String param) {
+        //上传一张照片，这个可以用来做UI自动化报错是截图，然后传到报告中去
+        pushPicture();
+        param = "这是zhangsan在登录测试……";
+        return param;
+    }
+
+
+    public void pushPicture() {
+        try {
+            Allure.addAttachment("picture", "image/png", new FileInputStream("/Users/apple/Desktop/photo/allure/allure_1.png"), ".png");
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // 通过反射动态获取执行方法
+    public Object reflectionMethod(String methodName, String param) throws NoSuchMethodException, SecurityException,
+            IllegalAccessException, IllegalArgumentException, InvocationTargetException {
+        Method method = getClass().getMethod(methodName, String.class);
+        return method.invoke(this, param);
+    }
 }
--- src/test/java/com/testNG/testCase/LoginTestDataAllure.java
+++ src/test/java/com/testNG/testCase/LoginTestDataAllure.java
@@ -1,5 +1,6 @@
 package com.testNG.testCase;
 
+import com.allure.SetStep;
 import io.qameta.allure.*;
 import io.qameta.allure.junit4.DisplayName;
 import org.testng.Assert;
@@ -8,17 +9,30 @@ import org.testng.annotations.Test;
 
 public class LoginTestDataAllure {
 
-    @DisplayName("Allure_test_demo_1")//case显示名称
-    @Description("这是第一个测试")//case说明
+    @DisplayName("Allure_test_demo")//case显示名称
+    @Description("DataProvider+allure测试")//case说明
     @Link("https://www.baidu.com")//case链接
     @Issue("001") //case编号
     @Severity(SeverityLevel.MINOR) //优先级[没看出什么用途]
     @Test(dataProvider = "putUsers", dataProviderClass = LoginTestDataAllure.class)
     public void testUserLoginData(String name,String pwd,String expect) {
-
         Login login = new Login();
         String ac = login.userLogin(name, pwd);
-        Assert.assertEquals(expect, ac);
+        boolean con = expect.equals(ac);
+
+        //如果预期不符合，可以动态添加step
+        if (!con){
+            SetStep ss = new SetStep();
+            if(expect.equals("用户名或者密码不能为空1")){
+                ss.stepAdd("otherERROR","loginByOther","");
+            }else if(expect.equals("欢迎管理员1")){
+                ss.stepAdd("loginByAdmin","loginByAdmin","");
+            }else if(expect.equals("欢迎zhangsan1")){
+                ss.stepAdd("loginByZhangSan","loginByZhangSan","");
+            }
+        }
+
+        Assert.assertTrue(con);
     }
 
 
@@ -28,9 +42,9 @@ public class LoginTestDataAllure {
                 {"", "", "用户名或者密码不能为空"},
                 {"admin", "", "用户名或者密码不能为空"},
                 {"zhangsan", "", "用户名或者密码不能为空"},
-                {"", "pwd", "用户名或者密码不能为空"},
-                {"admin", "123456", "欢迎管理员"},
-                {"zhangsan", "123456", "欢迎zhangsan"}
+                {"", "pwd", "用户名或者密码不能为空1"},
+                {"admin", "123456", "欢迎管理员1"},
+                {"zhangsan", "123456", "欢迎zhangsan1"}
         };
     }
 
